5f562c9b1142ca6e75ccafaa32714a885a946eb4
Telesphore <tuganimana01@gmail.comm>
2022-08-30 16:38:47 +0200

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React from  'react'
+
+// const Home = ()=>{
+//      return(<>
+//      <div>
+//          <p>Home page</p>
+//      </div>
+//      </>)
+// }
+
+// export default Home
+function Home(){
+    return(<>
+    <div><p>Home page</p></div>
+    </>)
+}
+
+export default Home
